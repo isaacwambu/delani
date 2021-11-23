eb15181100f17b4033b4582cfe203d9fea9677d1
isaacwambu <codeacademyj@gmail.com>
2021-11-23 22:09:19 +0300

    put images in a 2 rows and 4 columns

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,26 @@ $(document).ready(function(){
         
         
     });
+    $(".service2 img").click(function(){
+        $(".show2").toggle();
+        $(".hidden2").toggle();
+        
+    });
+    $(".service2 p").click(function(){
+        $("#hidden2").toggle();
+        $("#show2").toggle();
+        
+        
+    });
+    $(".service3 img").click(function(){
+        $(".show3").toggle();
+        $(".hidden3").toggle();
+        
+    });
+    $(".service3 p").click(function(){
+        $("#hidden3").toggle();
+        $("#show3").toggle();
+        
+        
+    });
 });
